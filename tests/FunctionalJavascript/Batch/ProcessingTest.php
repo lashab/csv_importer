1fd674cb7a34c920ee806242bf51dde277006080
lasha <lashabp@gmail.com>
2018-01-08 18:18:23 +0400

    Update tests

--- tests/FunctionalJavascript/Batch/ProcessingTest.php
+++ tests/FunctionalJavascript/Batch/ProcessingTest.php
@@ -4,51 +4,64 @@ namespace Drupal\csv_importer\FunctionalJavascript\Batch;
 
 use Drupal\Core\Url;
 use Drupal\FunctionalJavascriptTests\JavascriptTestBase;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\field\Entity\FieldConfig;
 
 /**
  * Tests batch processing in form and non-form workflow.
  *
- * @group Batch
+ * @group csv_importer
  */
 class ProcessingTest extends JavascriptTestBase {
 
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['csv_importer'];
+  protected static $modules = ['csv_importer', 'node', 'link', 'file', 'views', 'pathauto'];
 
   /**
    * {@inheritdoc}
    */
   protected function setUp() {
     parent::setUp();
+
+    $account = $this->drupalCreateUser(['access content overview', 'administer site configuration']);
+    $this->drupalLogin($account);
+    $this->drupalCreateContentType(['type' => 'csv_importer_test']);
+
+    FieldStorageConfig::create([
+      'field_name' => 'field_link',
+      'entity_type' => 'node',
+      'type' => 'link',
+    ])->save();
+    FieldConfig::create([
+      'field_name' => 'field_link',
+      'entity_type' => 'node',
+      'bundle' => 'csv_importer_test',
+    ])->save();
+
+    entity_get_display('node', 'csv_importer_test', 'default')
+      ->setComponent('field_link')
+      ->save();
+    entity_get_form_display('node', 'csv_importer_test', 'default')
+      ->setComponent('field_link')
+      ->save();
   }
 
   public function testCsvImporterPage() {
-    $account = $this->drupalCreateUser(['administer site configuration']);
-    $this->drupalLogin($account);
     $this->drupalGet('admin/config/development/csv-importer');
     $this->assertSession()->statusCodeEquals(200);
 
     $page = $this->getSession()->getPage();
-    $page->hasSelect('entity_type');
-    // $page->selectFieldOption('entity_type', 'Content');
-    // $page->waitForElement();
-    // $page->selectFieldOption('entity_type_bundle', 'Event');
-
-    // $page->findField('update');
-    // $page->checkField('update');
-    // $page->fillField('update_field', 'tid');
-    // $this->createScreenshot('/Users/machd/Desktop/screen.jpg');
-
-    // $this->getSession()->getPage()->fillField('entity_type', 'node');
-    // // $this->getSession()->getPage()->waitForElement();
-    // // $this->getSession()->getPage()->fillField('entity_type_bundle', 'event');
-
-    // $this->createScreenshot('/Users/machd/Desktop/screen.jpg');
-
-    $this->assertRaw(t('View'));
-    $this->assertRaw(t('Settings'));
+    $page->selectFieldOption('entity_type', 'node');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $page->selectFieldOption('entity_type_bundle', 'csv_importer_test');
+    $page->attachFileToField('files[csv]', drupal_get_path('module', 'csv_importer') . '/tests/csv_example_node_test.csv');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $page->pressButton('CSV import');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $this->drupalGet('/csv-importer-test-1');
+    $this->assertSession()->linkExists('CSV importer test link 1');
   }
 
 }
