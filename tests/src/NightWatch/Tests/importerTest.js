7b25a57157108d9cd2b7bdff01000c1e31cf0716
lashabp <lashabp@gmail.com>
2019-04-14 14:19:25 +0400

    Add Nightwatch tests

--- tests/src/NightWatch/Tests/importerTest.js
+++ tests/src/NightWatch/Tests/importerTest.js
@@ -1,5 +1,5 @@
-export default {
-  '@tags': ['csv_importer'],
+module.exports = {
+  '@tags': ['your_module'],
   before: function(browser) {
     browser
       .drupalInstall();
@@ -7,5 +7,17 @@ export default {
   after: function(browser) {
     browser
       .drupalUninstall();
-  }
+  },
+  'Visit a test page and create some test page': (browser) => {
+    browser
+      .drupalRelativeURL('/test-page')
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('body', 'Test page text')
+      .drupalRelativeURL('/node/add/page')
+      .setValue('input[name=title]', 'A new node')
+      .setValue('input[name="body[0][value]"]', 'The main body')
+      .click('#edit-submit')
+      .end();
+  },
+
 };
